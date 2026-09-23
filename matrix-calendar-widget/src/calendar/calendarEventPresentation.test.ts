d10ccad10868edb95f83997129b6b7b24c4d0806
0cwa <kainau@yahoo.com>
2026-09-23 11:09:42 +0000

    style: wrap calendar filter assertion

--- matrix-calendar-widget/src/calendar/calendarEventPresentation.test.ts
+++ matrix-calendar-widget/src/calendar/calendarEventPresentation.test.ts
@@ -84,7 +84,9 @@ describe('calendar event presentation', () => {
   });
 
   it('filters by title, description, location, or category', () => {
-    expect(filterCalendarEvents([timedEvent], 'quarterly')).toEqual([timedEvent]);
+    expect(filterCalendarEvents([timedEvent], 'quarterly')).toEqual([
+      timedEvent,
+    ]);
     expect(filterCalendarEvents([timedEvent], 'room 3')).toEqual([timedEvent]);
     expect(filterCalendarEvents([timedEvent], 'team')).toEqual([timedEvent]);
     expect(filterCalendarEvents([timedEvent], 'missing')).toEqual([]);
