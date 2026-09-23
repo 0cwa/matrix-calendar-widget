d17c1606e31e1e46a71993b6b75c93937609d764
0cwa <kainau@yahoo.com>
2026-09-23 10:33:11 +0000

    feat: add widget calendar repository query seam

---
+++ matrix-calendar-widget/src/calendar/CalendarRepositoryProvider.tsx
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2026 Matrix Calendar Widget contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { CalendarRepository } from '@matrix-calendar-widget/calendar';
+import {
+  PropsWithChildren,
+  createContext,
+  useContext,
+} from 'react';
+
+const CalendarRepositoryContext = createContext<CalendarRepository | undefined>(
+  undefined,
+);
+
+export function CalendarRepositoryProvider({
+  children,
+  repository,
+}: PropsWithChildren<{ repository: CalendarRepository }>) {
+  return (
+    <CalendarRepositoryContext.Provider value={repository}>
+      {children}
+    </CalendarRepositoryContext.Provider>
+  );
+}
+
+export function useCalendarRepository(): CalendarRepository {
+  const repository = useContext(CalendarRepositoryContext);
+
+  if (!repository) {
+    throw new Error(
+      'useCalendarRepository must be used inside CalendarRepositoryProvider',
+    );
+  }
+
+  return repository;
+}
